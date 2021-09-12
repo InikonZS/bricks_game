37e5cd29718ef225e5da810c04c79a094445d667
Inikon <ZhanZS@mail.ru>
2021-09-12 16:50:30 +0300

    refactor code

--- src/bricksCore/core.ts
+++ src/bricksCore/core.ts
@@ -2,35 +2,45 @@ export class Game{
   public field: Field;
   public stackList: StackList;
 
-  constructor(width:number, height:number, colors:number){
-    this.field = new Field(width, height, colors);
+  constructor(width:number, height:number, colors:number, breakFigureLength:number){
+    this.field = new Field(width, height, colors, breakFigureLength);
     this.stackList = new StackList(width, height, colors);
     this.field.onReverted = (cell:Cell)=>{
-      let st = this.stackList.stacks.find(stack=>{
-        let dir = [0, 4, 3, 2, 1];
-        return stack.initialPosition.x == cell.position.x && stack.initialPosition.y == cell.position.y && stack.direction == dir[cell.direction];
-      });
-      if (st){
-        st.push(cell.color);
-      }
+      const st = this.stackList.findByCell(cell);
+      st.push(cell.color);
     }
   }
 
   public move(stackIndex:number){
     const currentStack = this.stackList.stacks[stackIndex];
-    console.log(currentStack.initialPosition);
-    if (this.field.cells.find(cell=>(((currentStack.direction == 1 || currentStack.direction == 4) && cell.position.x == currentStack.initialPosition.x) || ((currentStack.direction == 2 || currentStack.direction == 3) &&cell.position.y == currentStack.initialPosition.y )) )){
+    if (this.field.isAvailableLine(currentStack.direction, currentStack.initialPosition)){
       this.field.putCell(new Cell(currentStack.pop(), currentStack.direction, {...currentStack.initialPosition}));
     }
   }
 
-  public processMove(){
+  /*public processMove(){
     for (let i=0; i<10000;i++){
       if (!this.field.processStep()){
         return;
       };
     } 
     throw new Error('Max iterations, break');
+  }*/
+
+  public processSteps(handlers:{onStep:(next:()=>void)=>void, onFinish:()=>void}){
+    const processStep = ()=>{
+      return this.field.processStep()
+    }
+    if (processStep()){
+      handlers.onStep(()=>{
+        this.processSteps(handlers);
+      });
+    } else {
+      handlers.onStep(()=>{
+        handlers.onFinish();
+      });
+      return;
+    }
   }
 
   public checkFigure(point:IVector2){
@@ -38,16 +48,18 @@ export class Game{
   }
 }
 
-class Field{
+export class Field{
   public cells: Array<Cell> = [];
   private width: number;
   private height: number;
   public onReverted:(cell:Cell)=>void;
+  private breakFigureLength: number
 
-  constructor(width:number, height:number, colors:number){
+  constructor(width:number, height:number, colors:number, breakFigureLength:number){
+    this.breakFigureLength = breakFigureLength;
     this.width = width;
     this.height = height;
-    for (let i = 0; i< 10; i++){
+    for (let i = 0; i< 1; i++){
       let cell = new Cell(Math.floor(Math.random() * colors), 0, {x: Math.floor(Math.random() * width), y:Math.floor(Math.random() * height)});
       this.putCell(cell);
     }
@@ -63,13 +75,17 @@ class Field{
     return cell;
   }
 
+  private isInsideField(nextPosition:IVector2){
+    return (nextPosition.x >=0 && nextPosition.y>=0 && nextPosition.x<this.width && nextPosition.y<this.height);
+  }
+
   public processStep(){
     let isChanged = false;
     this.cells.forEach(cell=>{
       const nextPosition = cell.getNextPosition();
-      let nextCell = this.cells[this.getIndex(nextPosition.x, nextPosition.y)]
+      const nextCell = this.getCell(nextPosition);//this.cells[this.getIndex(nextPosition.x, nextPosition.y)]
       if ( nextCell == null ){
-        if (nextPosition.x >=0 && nextPosition.y>=0 && nextPosition.x<this.width && nextPosition.y<this.height){
+        if (this.isInsideField(nextPosition)){
           cell.position = nextPosition;
         } else {
           this.revertCell(cell);
@@ -81,7 +97,7 @@ class Field{
     if (isChanged == false){
       this.cells.forEach(cell=>{
         let fig = this.checkFigure(cell.position, cell.color);
-        if (fig.length>3){
+        if (fig.length>=this.breakFigureLength){
           this.removeFigure(fig);
           isChanged = true;
         }
@@ -90,15 +106,19 @@ class Field{
     return isChanged;
   }
 
-  private getIndex(x:number, y:number):number{
+  public getIndex(x:number, y:number):number{
     return this.cells.findIndex(cell=>{
       return cell.position.x == x && cell.position.y == y;
     });
   }
 
-  public checkFigure(initialPoint:IVector2, color:number){
-    let waveField = this.cells.map(it=>{
-      return {color:it.color, generation:99999}
+  public getCell(position:IVector2):Cell{
+    return this.cells[this.getIndex(position.x, position.y)];
+  }
+
+  private checkFigure(initialPoint:IVector2, color:number){
+    const waveField = this.cells.map(it=>{
+      return {color:it.color, generation: Number.MAX_SAFE_INTEGER}
     });
 
     const moves:Array<IVector2> = [{x:0, y:1},{x:1, y:0}, {x:-1, y:0}, {x:0, y:-1}];
@@ -130,13 +150,28 @@ class Field{
   public removeFigure(figPoints:Array<IVector2>){
     let deletedCells:Array<Cell> = [];
     figPoints.forEach(point=>{
-        let cell = this.cells[this.getIndex(point.x, point.y)];
+        let cell = this.getCell(point);//this.cells[this.getIndex(point.x, point.y)];
         if (cell){
           deletedCells.push(cell);
         }
     });
 
     this.cells = this.cells.filter(it=>!deletedCells.includes(it));
+  }
+
+  public isAvailableLine(direction: number, initialPosition: IVector2):boolean{
+    const firstCell = this.cells.find(cell=>{
+      return (
+        ((direction == 1 || direction == 4) && cell.position.x == initialPosition.x) || 
+        ((direction == 2 || direction == 3) && cell.position.y == initialPosition.y)
+      )
+    });
+    if (firstCell){
+      if (!this.getCell(initialPosition)){
+        return true;
+      }
+    }
+    return false;
   }
 
 }
@@ -147,7 +182,7 @@ class StackList{
   constructor (width:number, height:number, colors:number){
     this.stacks = [];
     for (let i = 0; i< (width + height) *2; i++){
-      const stack = new Stack();
+      const stack = new Stack(colors);
       const direction = this.getDirection(i, width, height);
       stack.direction = direction;
       stack.initialPosition = this.getInitial(i, width, height);
@@ -172,25 +207,39 @@ class StackList{
     return {x: Math.floor(index % width), y: height - 1}
   }
 
+  public findByCell(cell:Cell){
+    return this.stacks.find(stack=>{
+      let dir = [0, 4, 3, 2, 1];
+      return stack.initialPosition.x == cell.position.x && stack.initialPosition.y == cell.position.y && stack.direction == dir[cell.direction];
+    });
+  }
+
  /* public getStack(index:number){
     return this.stacks[index];
   }*/
 
 }
 
-class Stack{
+export class Stack{
   public cells: Array<number> = [];
   public direction: number;
   public initialPosition: IVector2;
 
+  private colorsCount: number;
+  private stackLength: number = 3;
+
+  constructor(colorsCount: number){
+    this.colorsCount = colorsCount;
+  }
+
   pop(){
     const cell = this.cells.pop();
-    this.cells.unshift(Math.floor(Math.random() * /*colors*/3));
+    this.cells.unshift(Math.floor(Math.random() * this.colorsCount));
     return cell;
   }
 
   push(color:number){
-    this.cells = this.cells.slice(this.cells.length - 2);
+    this.cells = this.cells.slice(this.cells.length + 1 - this.stackLength);
     return this.cells.push(color); 
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,95 @@
-import { Game } from './bricksCore/core';
+import { Game , Stack, Field} from './bricksCore/core';
 import Control from './control/control';
 import './index.css';
 
+class StackView extends Control{
+  private stackModel:Stack;
+  private cellViews: Array<Control>;
+  private colors:Array<string>;
+
+  constructor (parentNode: HTMLElement, stackModel:Stack, colors:Array<string>, onClick:()=>void){
+    let direction = stackModel.direction;
+    super(parentNode, 'div', (direction == 2 || direction == 3)?'row':'row row__vertical');
+    
+    this.cellViews = [];
+    this.stackModel = stackModel;
+    this.colors = colors;
+
+    stackModel.cells.forEach((jt, j)=>{
+      let cellView = new Control(this.node, 'div', 'cell cell__stack');
+      cellView.node.textContent = direction.toString();
+      cellView.node.onclick = ()=>{
+        onClick();
+      }
+      this.cellViews.push(cellView);
+
+      /*cellView.node.onmouseover = ()=>{
+        cells[it.initialPosition.y][it.initialPosition.x].node.style.borderWidth = '3px';
+      }
+      cellView.node.onmouseout = ()=>{
+        cells[it.initialPosition.y][it.initialPosition.x].node.style.borderWidth = '';
+      }*/
+
+      
+    });  
+
+    //this.update();
+  }
+
+  public update(){
+    this.cellViews.forEach((cellView, index)=>{
+      const direction = this.stackModel.direction
+      const cellIndex = ( direction == 2 || direction == 1 ) ? index : this.stackModel.cells.length - 1 - index
+      const cellColor = this.stackModel.cells[cellIndex]
+      cellView.node.style.backgroundColor = this.colors[cellColor];
+    });
+  }
+}
+
+class FieldView extends Control{
+  private colors:Array<string>;
+  private fieldModel:Field;
+  private cells: Array<Array<Control>>;
+
+  constructor (parentNode:HTMLElement, colors:Array<string>, fieldModel:Field){
+    super(parentNode);
+    this.fieldModel = fieldModel;
+    this.colors = colors;
+    this.cells = [];
+    for (let i = 0; i<10; i++){
+      const rowView = new Control(this.node, 'div', 'row');
+      const row: Array<Control> = [];
+      for (let j = 0; j< 10; j++){
+        let cell = new Control(rowView.node, 'div', 'cell cell__field');  
+        row.push(cell); 
+      }
+      this.cells.push(row);
+    }
+  }
+
+  public update(){
+    this.cells.forEach(it=>it.forEach(cellView=>{
+      cellView.node.style.backgroundColor = '#fff';
+      cellView.node.textContent = '';
+    }));
+
+    this.fieldModel.cells.forEach(cell=>{
+      const cellView = this.cells[cell.position.y][cell.position.x];
+      cellView.node.style.backgroundColor = this.colors[cell.color];
+      cellView.node.textContent = cell.direction.toString();
+    }); 
+  }
+}
+
 class BricksView extends Control{
+  private colors = ['#f99', '#9f9', '#99f', '#f4f', '#df0', '#f90'];
+  private fieldView: FieldView;
+  private stakes: Array<StackView>;
+
   constructor (parentNode: HTMLElement, game:Game){
     super(parentNode, 'div', 'grid_wrapper');
 
+    this.stakes = [];
     const gridZones:Array<Control> = [];
     for (let i =0; i< 9; i++){
       gridZones.push(new Control(this.node, 'div', 'grid_zone'));
@@ -15,19 +99,16 @@ class BricksView extends Control{
       const stackView = new Control(gridZones[[1, 3, 5, 7][direction -1 ]].node, 'div', (direction == 1 || direction == 4)?'row':'row row__vertical');
       game.stackList.stacks.forEach((it, i)=>{
         //console.log(it.direction);
-        if (it.direction == direction){
+        /*if (it.direction == direction){
           let stacker = new Control(stackView.node, 'div', (direction == 2 || direction == 3)?'row':'row row__vertical');
           
           it.cells.forEach((jt, j)=>{
 
           
-            let cellView = new Control(stacker.node, 'div', 'cell');
+            let cellView = new Control(stacker.node, 'div', 'cell cell__stack');
             cellView.node.textContent = it.direction.toString();
             cellView.node.onclick = ()=>{
-              game.move(i);
-              game.processMove();
-              document.querySelector('#app').innerHTML='';
-              new BricksView(document.querySelector('#app'), game);
+              this.handleMove(i);
             }
 
             cellView.node.onmouseover = ()=>{
@@ -37,12 +118,16 @@ class BricksView extends Control{
               cells[it.initialPosition.y][it.initialPosition.x].node.style.borderWidth = '';
             }
 
-            cellView.node.style.backgroundColor = ['#f99', '#9f9', '#99f', '#f9f'][it.cells[(direction == 2 || direction == 1) ? j : it.cells.length-1 - j]];
+            cellView.node.style.backgroundColor = this.colors[it.cells[(direction == 2 || direction == 1) ? j : it.cells.length-1 - j]];
           });
+        }*/
+        if (it.direction == direction){
+          let stacker = new StackView(stackView.node, it, this.colors, ()=>{ this.handleMove(i); });
+          this.stakes.push(stacker);
         }
       });
     }
-    
+    /*
 
     const cells: Array<Array<Control>> = [];
     const fieldView = new Control(gridZones[4].node);
@@ -50,7 +135,7 @@ class BricksView extends Control{
       const rowView = new Control(fieldView.node, 'div', 'row');
       const row: Array<Control> = [];
       for (let j = 0; j< 10; j++){
-        let cell = new Control(rowView.node, 'div', 'cell');  
+        let cell = new Control(rowView.node, 'div', 'cell cell__field');  
         row.push(cell); 
       }
       cells.push(row);
@@ -58,13 +143,43 @@ class BricksView extends Control{
 
     game.field.cells.forEach(cell=>{
       const cellView = cells[cell.position.y][cell.position.x];
-      cellView.node.style.backgroundColor = ['#f99', '#9f9', '#99f', '#f9f'][cell.color];
+      cellView.node.style.backgroundColor = this.colors[cell.color];
       cellView.node.textContent = cell.direction.toString();
+    });*/
+
+    this.fieldView = new FieldView(gridZones[4].node, this.colors, game.field);
+
+    this.update();
+  }
+
+  private handleMove(stackIndex:number){
+    game.move(stackIndex);
+    //game.processMove();
+    let timer = 0;
+    game.processSteps({
+      onStep:(next)=>{
+        setTimeout(()=>{
+          document.querySelector('#app').innerHTML='';
+          new BricksView(document.querySelector('#app'), game);
+          timer = 100;
+          next();
+        }, timer);
+      },
+      onFinish:()=>{
+          if (game.field.cells.length == 0){
+            console.log('win');
+          }
+      }
     });
+  }
+
+  public update(){
+    this.stakes.forEach(stake=>stake.update());
+    this.fieldView.update();
   }
 }
 
-const game = new Game(10, 10, 3);
+const game = new Game(10, 10, 6, 3);
 
 new BricksView(document.querySelector('#app'), game);
 
